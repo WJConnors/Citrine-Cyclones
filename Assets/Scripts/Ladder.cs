6c7f25971bb4660b536c8bf78d79b7457724ca5c
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-01-10 18:34:45 -0500

    clean up code and prefabs

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
+    private const string PLAYER_TAG = "Player";
+    public List<SpriteRenderer> ladderSprites;
+    private BoxCollider2D ladderCollider;
+    private void Awake()
+    {
+        ladderCollider = GetComponent<BoxCollider2D>();
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag(PLAYER_TAG))
         {
             collision.gameObject.GetComponent<BaseCharacter>().isClimbing = true;
         }
@@ -14,9 +21,18 @@ public class Ladder : MonoBehaviour
 
     public void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag(PLAYER_TAG))
         {
             collision.gameObject.GetComponent<BaseCharacter>().isClimbing = false;
+        }
+    }
+
+    public void TurnOnLadder()
+    {
+        ladderCollider.enabled = true;
+        foreach (var sprite in ladderSprites)
+        {
+            sprite.enabled = true;
         }
     }
 }
