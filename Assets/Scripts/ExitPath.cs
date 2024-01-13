f0cc7da1ae17706958d5b50e42881c5870412029
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-01-12 21:52:33 -0500

    add exit path

---
+++ Assets/Scripts/ExitPath.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitPath : MonoBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+    private int playersInExit = 0;
+    private const int PLAYERS_NEEDED_IN_EXIT = 2;
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag(PLAYER_TAG))
+            return;
+
+        playersInExit++;
+
+        if(playersInExit == PLAYERS_NEEDED_IN_EXIT)
+        {
+            GoToNextLevel();
+        }
+    }
+
+    private void GoToNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        currentScene++;
+        if(currentScene == SceneManager.sceneCount)
+        {
+            //no more next scene 
+            //you win game
+        }
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag(PLAYER_TAG))
+            return;
+
+        playersInExit--;
+    }
+
+    private void Update()
+    {
+        //debuging help
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
